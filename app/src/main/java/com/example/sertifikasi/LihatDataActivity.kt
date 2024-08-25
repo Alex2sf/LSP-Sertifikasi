4a7ed1c3bbaa9f498c369538143bc3988bd2534f
Alex Zaro Daeli <alexzarodaeli8@gmail.com>
2024-08-25 16:04:55 +0700

    benerin

--- app/src/main/java/com/example/sertifikasi/LihatDataActivity.kt
+++ app/src/main/java/com/example/sertifikasi/LihatDataActivity.kt
@@ -9,12 +9,9 @@ import androidx.core.view.WindowInsetsCompat
 class LihatDataActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_lihat_data)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        enableEdgeToEdge() // Untuk mengaktifkan tampilan edge-to-edge
+        setContentView(R.layout.activity_lihat_data) // Mengatur layout dari XML
+
+
     }
 }
